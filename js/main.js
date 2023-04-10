9bdd6c172874a364e2ded97f54dd3b692a887e0e
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-04-10 14:11:11 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const playBtn = document.querySelector('.stopwatch__btn__option--start');
+const pauseBtn = document.querySelector('.stopwatch__btn__option--pause');
+const stopBtn = document.querySelector('.stopwatch__btn__option--stop');
+const resetBtn = document.querySelector('.stopwatch__btn__option--reset');
+const historyBtn = document.querySelector('.stopwatch__btn__option--history');
+const clock = document.querySelector('.stopwatch__time');
+const LastTime = document.querySelector('.stopwatch__result');
+const timeList = document.querySelector('.stopwatch__list');
+const infoBtn = document.querySelector('.stopwatch__info');
+const modalShadow = document.querySelector('.modal');
+const modalBtn = document.querySelector('.close');
+
+const timePlay = () => {
+	let minutes = minutes++;
+};
+// timePlay();
